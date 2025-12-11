7bfb20129f24efa15f5a3910fa5dd84525ea5495
Naywasd <sermsub2549@gmail.com>
2025-12-11 12:45:14 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,27 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int x = 1;
+    int even = 0;
+    int odd = 0;
+
+    while (x != 0) {
+        cout << "Enter an integer: ";
+        cin >> x;
+
+        if(x == 0){
+            break;
+        }
+
+        if(x%2 == 0){
+            even++;
+        } else {
+            odd++;
+        }
+    }
+
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd << endl;
+    
     return 0;
 }
